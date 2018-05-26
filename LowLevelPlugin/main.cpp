23d54b697191242aa9190fe2a2c2c26a8745971f
Moon Antonio <pinchodev91@gmail.com>
2018-05-26 04:01:32 +0200

    Agregado codigo fuente

--- LowLevelPlugin/main.cpp
+++ LowLevelPlugin/main.cpp
@@ -1,31 +1,20 @@
-#include "main.h"
+#include <stdlib.h>
+#include <math.h>
+#include "LowLevelPlugin.h"
 
-// a sample exported function
-void DLL_EXPORT SomeFunction(const LPCSTR sometext)
+extern "C" int ** EXPORT_API fillArray(int size)
 {
-    MessageBoxA(0, sometext, "DLL Message", MB_OK | MB_ICONINFORMATION);
-}
+    int i = 0, j = 0;
+    int ** array = (int**) calloc(size, sizeof(int*));
 
-extern "C" DLL_EXPORT BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-{
-    switch (fdwReason)
+    for(i = 0; i < size; i++)
     {
-        case DLL_PROCESS_ATTACH:
-            // attach to process
-            // return FALSE to fail DLL load
-            break;
-
-        case DLL_PROCESS_DETACH:
-            // detach from process
-            break;
-
-        case DLL_THREAD_ATTACH:
-            // attach to thread
-            break;
-
-        case DLL_THREAD_DETACH:
-            // detach from thread
-            break;
+        array[i] = (int*) calloc(size, sizeof(int));
+        for(j = 0; j < size; j++)
+        {
+            array[i][j] = i * size + j;
+        }
     }
-    return TRUE; // succesful
+
+    return array;
 }
